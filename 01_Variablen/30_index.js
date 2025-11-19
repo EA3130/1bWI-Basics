ab0c9455b3c361b03374b2a4bcc1d1cc05999fc3
efe <you@example.com>
2025-11-19 16:22:15 +0100

    30_Variablen fertig

--- 01_Variablen/30_index.js
+++ 01_Variablen/30_index.js
@@ -1,5 +1,19 @@
-Let apple = 0.34
-let banana = 0.22
-let 2.14 = 1
-let 3.43 = 1
-console.log(apple * 5)
+let apple = "Apple";
+let banana = "Banana";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleweight = 0.34;
+let bananaweight = 0.22;
+
+let appleweight1 = 1 / appleweight;
+let bananaweight1 = 1 / bananaweight;
+
+let applePrice = appleweight1  / applePricePerKilo;
+let bananaprice = bananaweight1  / bananaPricePerKilo;
+
+console.log("Preis von 8 äpfel" + " " + 8 * applePrice + " " + "Euro");
+console.log("Preis von 17 bananen" + " " + 17 * bananaprice + " " + "Euro");
+console.log("Preis von 1 tonne Äpfel" + " " + 1000 * applePrice + " " + "Euro");
+console.log("Preis von 1 tonne Bananen" + " " + 1000 * bananaprice + " " + "Euro");
