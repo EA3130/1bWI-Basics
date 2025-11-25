861b4889ce370422b2fcf6a392ab7fed6994ecd4
efe <you@example.com>
2025-11-25 18:07:56 +0100

    40_if_aufgabe2

---
+++ 40_if_bedingungen/aufgabe2.js
@@ -0,0 +1,10 @@
+let trafficLight = Green
+if (trafficLight == Green) {
+    console.log("You can go");
+}
+if (trafficLight == Yellow) {
+    console.log("Caution!");
+}
+if (trafficLight == Red) {
+    console.log("stop!");
+}
