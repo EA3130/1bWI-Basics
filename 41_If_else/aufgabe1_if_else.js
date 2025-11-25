2df8043ad38a4f898b676768635d6169c5959d98
efe <you@example.com>
2025-11-25 18:20:45 +0100

    Aufgabe1_if_else

---
+++ 41_If_else/aufgabe1_if_else.js
@@ -0,0 +1,13 @@
+let a = 10
+// a > 10 größer
+// a < 10 kleiner
+// a >= 10 größer gleich
+// a <= 10 kleiner gleich
+
+if (a >= 0) {
+    console.log("the number is Positive");
+} else (a <= 0) {
+    console.log("the number is Negative");
+} else (a == 0) {
+    console.log("the number is zero");
+}
