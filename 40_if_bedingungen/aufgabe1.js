d6b78c56c806a87d956845216da597f873dab588
efe <you@example.com>
2025-11-25 17:58:23 +0100

    40_if_Aufgabe 1

--- 40_if_bedingungen/aufgabe1.js
+++ 40_if_bedingungen/aufgabe1.js
@@ -1,1 +1,10 @@
-console.log("test");
+let a = 7
+if (a > 0) {
+    console.log("the number is positive");
+}
+if (a < 0) {
+    console.log("the number is negative");
+}
+if (a == 0) {
+    console.log("the number is zero");
+}
