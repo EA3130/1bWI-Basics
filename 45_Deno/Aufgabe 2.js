2b867dbcfe0917232dea66b21bf28a980ce9d46e
efe <you@example.com>
2025-12-17 16:04:51 +0100

    45DenoAufgabe2

---
+++ 45_Deno/Aufgabe 2.js
@@ -0,0 +1,11 @@
+let a = Math.floor(Math.random() * 10);
+console.log(a);
+
+if (a < 0) {
+    console.log("the number is negative");
+} else if (a > 0) {
+    console.log("the number is positive");
+} else {
+    console.log("the number is 0");
+}
+
---
+++ Übung/Übung.js
@@ -0,0 +1,20 @@
+let variable = prompt("Bitte gib eine Zahl ein");
+
+if (variable > 0) {
+    console.log("Die Zahl ist positiv");
+} else if (variable < 0) {
+    console.log("Die Zahl ist negativ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
