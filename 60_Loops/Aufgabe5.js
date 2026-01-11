a83ccb9e3b4363f6c006a1d8b9527ceb8b99ea23
efe <you@example.com>
2026-01-11 14:30:22 +0100

    60_loops_aufgabe5

---
+++ 60_Loops/Aufgabe5.js
@@ -0,0 +1,6 @@
+let firstNumber = parseInt(prompt("enter first number"));
+let secondNumber = parseInt(prompt("enter second number"));
+
+for (let i = firstNumber; i <= secondNumber; i++) {
+    console.log(i);
+}
